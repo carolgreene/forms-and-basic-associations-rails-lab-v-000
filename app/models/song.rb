36a67143a58232f9c245d7372a86ec2a2199bcba
Carol Greene <carolgreene3@gmail.com>
2018-06-02 14:10:21 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,35 +5,6 @@ class Song < ActiveRecord::Base
 
 
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
-  end
 
-  def artist_name
-    self.artist ? self.artist.name : nil
-  end
-
-  def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
-  end
-
-  def genre_name
-    self.genre.name
-  end
-
-  def note_contents=(contents)
-    contents.each do |content|
-
-      if content.strip != ""
-        self.notes.build(content: content)
-      end
-    end
-  end
-
-  def note_contents
-    self.notes.collect do |note|
-      note.content
-    end
-  end
 
 end
